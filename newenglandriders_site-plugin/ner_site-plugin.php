371c164815811f8b291631003f6b8e13ee6b382d
Steve Gallant <sjgallant@hotmail.com>
2019-08-04 13:48:45 -0400

    Added Route Size custom taxonomy.

--- newenglandriders_site-plugin/ner_site-plugin.php
+++ newenglandriders_site-plugin/ner_site-plugin.php
@@ -3,6 +3,36 @@
 Plugin Name: Site Plugin for newenglandriders.org
 Description: Site specific code changes for newenglandriders.org
 */
+// hook into the init action and call create_book_taxonomies when it fires
+add_action('init', 'create_routesize_hierarchical_taxonomy', 0);
 
+// Create a custom taxonomy named 'Route Sizes'
+function create_routesize_hierarchical_taxonomy() {
+  // Add new taxonomy, make it hierarchical
+  // First do the translations part for GUI
+  $labels = array(
+    'name' => _x('Route Sizes', 'taxonomy general name'),
+    'singular_name' => _x('Route Size', 'taxonomy singular name'),
+    'search_items' => __('Search Route Sizes'),
+    'all_items' => __('All Route Sizes'),
+    'parent_item' => __('Parent Route Size'),
+    'parent_item_colon' => __('Parent Route Size:'),
+    'edit_item' => __('Edit Route Size'),
+    'update_item' => __('Update Route Size'),
+    'add_new_item' => __('Add New Route Size'),
+    'new_item_name' => __('New Route Size Name'),
+    'menu_name' => __('Route Sizes'),
+  );
+
+  // Register the taxonomy
+  register_taxonomy('routesize',array('page'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array('slug' => 'routesize'),
+  ));
+}
 
 ?>
