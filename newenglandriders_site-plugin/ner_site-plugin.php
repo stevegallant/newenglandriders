2c870e0bbbb9d83253d0d227b1429aa2e6467ae2
Steve Gallant <sjgallant@hotmail.com>
2019-08-09 10:26:06 -0400

    Add Locale taxonomy. Add more files for version control.

--- newenglandriders_site-plugin/ner_site-plugin.php
+++ newenglandriders_site-plugin/ner_site-plugin.php
@@ -2,36 +2,66 @@
 /*
 Plugin Name: Site Plugin for newenglandriders.org
 Description: Site specific code changes for newenglandriders.org
+Author: S. Gallant
 */
 // hook into the init action and call create_book_taxonomies when it fires
 add_action('init', 'create_routesize_hierarchical_taxonomy', 0);
 
 // Create a custom taxonomy named 'Route Sizes'
-function create_routesize_hierarchical_taxonomy() {
+function create_routescale_hierarchical_taxonomy() {
   // Add new taxonomy, make it hierarchical
   // First do the translations part for GUI
   $labels = array(
-    'name' => _x('Route Size', 'taxonomy general name'),
-    'singular_name' => _x('Route Size', 'taxonomy singular name'),
-    'search_items' => __('Search Route Size'),
-    'all_items' => __('All Route Sizes'),
-    'parent_item' => __('Parent Route Size'),
-    'parent_item_colon' => __('Parent Route Size:'),
-    'edit_item' => __('Edit Route Size'),
-    'update_item' => __('Update Route Size'),
-    'add_new_item' => __('Add New Route Size'),
-    'new_item_name' => __('New Route Size Name'),
-    'menu_name' => __('Route Size'),
+    'name' => _x('Route Scale', 'taxonomy general name'),
+    'singular_name' => _x('Route Scale', 'taxonomy singular name'),
+    'search_items' => __('Search Route Scale'),
+    'all_items' => __('All Route Scales'),
+    'parent_item' => __('Parent Route Scale'),
+    'parent_item_colon' => __('Parent Route Scale:'),
+    'edit_item' => __('Edit Route Scale'),
+    'update_item' => __('Update Route Scale'),
+    'add_new_item' => __('Add New Route Scale'),
+    'new_item_name' => __('New Route Scale Name'),
+    'menu_name' => __('Route Scale'),
   );
 
   // Register the taxonomy
-  register_taxonomy('routesize',array('page'), array(
+  register_taxonomy('routescale',array('page'), array(
     'hierarchical' => true,
     'labels' => $labels,
     'show_ui' => true,
     'show_admin_column' => true,
     'query_var' => true,
-    'rewrite' => array('slug' => 'routesize'),
+    'rewrite' => array('slug' => 'routescale'),
+  ));
+}
+
+// Create a custom taxonomy named 'Locale'
+function create_locale_hierarchical_taxonomy() {
+  // Add new taxonomy, make it hierarchical
+  // First do the translations part for GUI
+  $labels = array(
+    'name' => _x('Locale', 'taxonomy general name'),
+    'singular_name' => _x('Locale', 'taxonomy singular name'),
+    'search_items' => __('Search Locale'),
+    'all_items' => __('All Locales'),
+    'parent_item' => __('Parent Locale'),
+    'parent_item_colon' => __('Parent Locale:'),
+    'edit_item' => __('Edit Locale'),
+    'update_item' => __('Update Locale'),
+    'add_new_item' => __('Add New Locale'),
+    'new_item_name' => __('New Locale Name'),
+    'menu_name' => __('Locale'),
+  );
+
+  // Register the taxonomy
+  register_taxonomy('locale',array('page'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array('slug' => 'locale'),
   ));
 }
 
---
+++ travelify-child/functions.php
@@ -0,0 +1,159 @@
+<?php
+
+
+function theme_enqueue_styles() {
+	$parent_style = 'travelify-style'; 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+	);
+		
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function customize_travelify_header(){
+remove_action( 'travelify_header', 'travelify_headerdetails' );
+add_action( 'travelify_header', 'custom_travelify_headerdetails', 99 );
+}
+add_action('init', 'customize_travelify_header');
+/**
+ * Shows Header Part Content
+ *
+ * Shows the site logo, title, description, searchbar, social icons etc.
+ */
+function custom_travelify_headerdetails() {
+?>
+	<?php
+		global $travelify_theme_options_settings;
+   	$options = $travelify_theme_options_settings;
+
+   	$elements = array();
+		$elements = array(
+			$options[ 'social_facebook' ],
+			$options[ 'social_twitter' ],
+			$options[ 'social_googleplus' ],
+			$options[ 'social_linkedin' ],
+			$options[ 'social_pinterest' ],
+			$options[ 'social_youtube' ],
+			$options[ 'social_vimeo' ],
+			$options[ 'social_flickr' ],
+			$options[ 'social_tumblr' ],
+			$options[ 'social_instagram' ],
+			$options[ 'social_rss' ],
+			$options[ 'social_github' ]
+		);
+
+		$flag = 0;
+		if( !empty( $elements ) ) {
+			foreach( $elements as $option) {
+				if( !empty( $option ) ) {
+					$flag = 1;
+				}
+				else {
+					$flag = 0;
+				}
+				if( 1 == $flag ) {
+					break;
+				}
+			}
+		}
+	?>
+
+	<div class="container clearfix">
+		<div class="hgroup-wrap clearfix">
+					<section class="hgroup-right">
+						<?php travelify_socialnetworks( $flag ); ?>
+					</section><!-- .hgroup-right -->
+				<hgroup id="site-logo" class="clearfix">
+					<?php
+						if( $options[ 'header_show' ] != 'disable-both' && $options[ 'header_show' ] == 'header-text' ) {
+						?>
+							<h1 id="site-title">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+									<?php bloginfo( 'name' ); ?>
+								</a>
+							</h1>
+							<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+						<?php
+						}
+						elseif( $options[ 'header_show' ] != 'disable-both' && $options[ 'header_show' ] == 'header-logo' ) {
+						?>
+							<h1 id="site-title">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+									<img src="<?php echo $options[ 'header_logo' ]; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+								</a>
+							</h1>
+						<?php
+						} else if( $options[ 'header_show' ] == 'disable-both'){ ?>
+							<h1 class="site-title">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+									<img src="<?php echo $options[ 'header_logo' ]; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+								</a>
+							</h1>
+							<h1 id="site-title">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+									<?php bloginfo( 'name' ); ?>
+								</a>
+							</h1>
+							<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+						<?php }
+						?>
+
+				</hgroup><!-- #site-logo -->
+
+		</div><!-- .hgroup-wrap -->
+	</div><!-- .container -->
+	<?php $header_image = get_header_image();
+			if( !empty( $header_image ) ) :?>
+				<img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+			<?php endif; ?>
+	<?php
+		if ( has_nav_menu( 'primary' ) ) {
+			$args = array(
+				'theme_location'    => 'primary',
+				'container'         => '',
+				'items_wrap'        => '<ul class="root">%3$s</ul>'
+			);
+			echo '<nav id="main-nav" class="clearfix">
+					<div class="container clearfix">';
+				wp_nav_menu( $args );
+			echo '</div><!-- .container -->
+					</nav><!-- #main-nav -->';
+		}
+		else {
+			echo '<nav id="main-nav" class="clearfix">
+					<div class="container clearfix">';
+				wp_page_menu( array( 'menu_class'  => 'root' ) );
+			echo '</div><!-- .container -->
+					</nav><!-- #main-nav -->';
+		}
+	?>
+		<?php
+		if( is_home() || is_front_page() ) {
+			if( "0" == $options[ 'disable_slider' ] ) {
+				if( function_exists( 'travelify_pass_cycle_parameters' ) )
+   				travelify_pass_cycle_parameters();
+   			if( function_exists( 'travelify_featured_post_slider' ) )
+   				travelify_featured_post_slider();
+   		}
+   		}
+		else {
+			if( ( '' != travelify_header_title() ) || function_exists( 'bcn_display_list' ) ) {
+		?>
+			<div class="page-title-wrap">
+	    		<div class="container clearfix">
+	    			<?php
+		    		if( function_exists( 'travelify_breadcrumb' ) )
+						travelify_breadcrumb();
+					?>
+				   <h3 class="page-title"><?php echo travelify_header_title(); ?></h3><!-- .page-title -->
+				</div>
+	    	</div>
+	   <?php
+	   	}
+		}
+}
+?>
