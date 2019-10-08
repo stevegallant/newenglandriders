f850974edb2ac2a49d131259c818bcb07851e6cb
Steve Gallant <sjgallant@hotmail.com>
2019-10-08 13:19:40 -0400

    Create skeleton folders for adding js to project.

---
+++ travelify-child/js/scripts.js
@@ -0,0 +1,8 @@
+// 3rd party packages from NPM
+import $ from 'jquery';
+
+// Custom modules and classes
+import LocaleChooser from './modules/LocaleChooser.js';
+
+// Instantiate a new object using module/class
+var localeChooser = new LocaleChooser();
