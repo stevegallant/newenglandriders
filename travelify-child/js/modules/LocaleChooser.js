7a54fbe3a3e677832d32546c9902da818ce038fe
Steve Gallant <sjgallant@hotmail.com>
2019-10-21 14:35:10 -0400

    Created new plugin NER Locale Chooser form and moved code from functions.php

---
+++ travelify-child/js/modules/LocaleChooser.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+
+// Contructor
+class LocaleChooser {
+  constructor() {
+    this.events();
+  }
+}
+// Events
+events() {
+
+}
+
+// Methods
+displayLocale() {
+
+}
+
+
+export default LocaleChooser;
